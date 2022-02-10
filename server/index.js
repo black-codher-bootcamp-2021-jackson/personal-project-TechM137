af68d10c06194026774b8d95804c27d05da1fec7
TechM137 <mulan137@DESKTOP-JF713R2.localdomain>
2022-02-10 21:26:34 +0000

    4 profiles up

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { param } = require("express/lib/request");
 
 // IMPORT YOUR SCHEMAS HERE
 require("./models/Profiles"); //This is just an example. Don't forget to delete this
+const profileRoutes = require("./routes/profilesRoutes");
 
 const app = express();
 
@@ -21,6 +23,24 @@ app.use(bodyParser.json());
 // Below is just an example. Don't forget to delete it. 
 // It's importing and using everything from the profilesRoutes.js file and also passing app as a parameter for profileRoutes to use
 require("./routes/profilesRoutes")(app); 
+
+// app.post(`/api/profile`, async (req, res) => {
+//   const profile = await Profile.create(req.body);
+
+//   return res.status(201).send({
+//     error: false,
+//     profile,
+//   });
+// });
+
+// app.post('/profilesService', (req, res) => {
+//   const { firstName, lastName, location } = req.params; 
+//   res.header("Content-Type", "application/json");
+//   res.status(202);
+
+//   res.send(profileServices);
+
+// })
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
